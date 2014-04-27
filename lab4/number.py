86c06119f85a3e5176765d0a43068925a701b5d8
Robin Deits <robin.deits@gmail.com>
2014-04-27 16:25:29 +0300

    finished lab4

--- lab4/number.py
+++ lab4/number.py
@@ -1,9 +1,24 @@
-class integer(object):
-	def __init__(self, number):
-		self.number = number
-	def display(self):
-		print self.number
+#define class-integer
+class integer (object):
+    def __init__(self, number, check):
+        self.number = number
+        self.check = check
+    def display(self):
+        print self.check + str(self.number)
+        #if self.check == "negative":
+        #    print self.number*-1
+        #else:
+        #   print self.number
+        #print self.check
+class negativeinteger (integer):
+    def __init__ (self, number):
+        super(negativeinteger, self).__init__(number, "negative")
+    def display(self):
+        integer.display(self)
+        print "this is an object of the Negativeinteger class"
 
 if __name__=="__main__":
-	d = integer(4)
-	d.display()
+    d = raw_input("give me a number:")
+    m = raw_input("give me another number:")
+    for x in [x,y]:
+        x.display()
